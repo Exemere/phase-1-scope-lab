19aca1a61339ad4a565a6405abc9e23b633cbc09
Livosero Esemere Nicolas <nesemere55@gmail.com>
2023-07-13 16:42:59 +0300

    completed assighnment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function bestCustomer() {
+  bestCustomer = '';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'undefined';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new least favorite';
+}
